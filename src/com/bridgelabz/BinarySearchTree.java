fd993529e5a3cdbfe87ef4b7f281db4241edd28b
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-27 07:03:04 +0530

    Added :Ability to create bst as shown in figure

--- src/com/bridgelabz/BinarySearchTree.java
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -2,11 +2,13 @@ package com.bridgelabz;
 
 public class BinarySearchTree {
     public static Node insert(Node root, int value) {
+        //comparison
         if (root == null) {
             root = new Node(value);
             return root;
         }
         if (root.data > value) {
+            // left-subtree
             root.left = insert(root.left, value);
         } else {
             root.right = insert(root.right, value);
@@ -14,25 +16,25 @@ public class BinarySearchTree {
         return root;
     }
 
-    public static void inOrder(Node root) {
+    public static void inorder(Node root) {
         if (root == null) {
             return;
         }
-        inOrder(root.left);
+        inorder(root.left);
         System.out.print(root.data + " ");
-        inOrder(root.right);
+        inorder(root.right);
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to binary search tree");
+        System.out.println("Welcome to binary search tree !");
 
-        int[] values = {56, 70, 30};
+        int values[] = {56, 30, 70, 22, 40, 11, 3, 16, 60, 95, 65, 63, 67};
 
         Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
         }
-        inOrder(root);
+        inorder(root);
         System.out.println();
 
     }
