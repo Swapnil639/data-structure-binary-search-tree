532ae9eb13923e114deef1410f7ba8d7d0f0a4a2
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-27 06:52:59 +0530

    Added :Ability to create binary search tree

--- src/com/bridgelabz/BinarySearchTree.java
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -1,9 +1,40 @@
 package com.bridgelabz;
 
 public class BinarySearchTree {
+    public static Node insert(Node root, int value) {
+        if (root == null) {
+            root = new Node(value);
+            return root;
+        }
+        if (root.data > value) {
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to binary search tree ");
+        System.out.println("Welcome to binary search tree");
+
+        int[] values = {56, 70, 30};
+
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inOrder(root);
+        System.out.println();
+
     }
 }
 
